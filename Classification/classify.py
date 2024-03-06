4204d7f614be43407ffc05dd5ebbb4258eb21917
chukotskiyShaman <s.mendrul@g.nsu.ru>
2024-03-06 15:13:36 +0700

    ?

---
+++ Classification/classify.py
@@ -0,0 +1,90 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+from PIL import Image
+from torch.utils.data import Dataset, DataLoader
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+
+class ConvNN(nn.Module):
+  def __init__(self):
+    super().__init__()
+    self.conv1 = nn.Conv2d(3, 32, kernel_size=(3,3))
+    self.conv2 = nn.Conv2d(32, 64, kernel_size=(3,3))
+    # self.f1 = nn.Flatten()
+    self.fc1 = nn.Linear(64*124*124, 128)
+    self.fc2 = nn.Linear(128, 42)
+
+  def forward(self, x):
+    x = F.relu(self.conv1(x))
+    x = F.relu(self.conv2(x))
+    # x = self.f1(x)
+    x = x.view(-1, 64*124*124)
+    x = F.relu(self.fc1(x))
+    x = self.fc2(x)
+    return x
+
+data_path = "./Data/simpsons_dataset"
+trans = transforms.Compose([transforms.Resize((128, 128)), transforms.ToTensor()])
+dataset = datasets.ImageFolder(data_path, transform=trans)
+
+data_path = "./testset"
+testset = datasets.ImageFolder(data_path, transform=trans)
+
+test_loader = DataLoader(testset, batch_size=32, shuffle=True)
+
+labels = ['Дед Абрахам',
+          'Агнес Скиннер',
+          'Апу индус',
+          'Барни Гамбли',
+          'Барт',
+          'Карл Карлсон',
+          'Чарльз Бёрнс',
+          'Шеф Виггум',
+          'Клетус',
+          'Чел с комиксами',
+          'Диско Стью',
+          'Една Крабаппел',
+          'Толстяк Тони',
+          'Гил',
+          'Вилли',
+          'Гомер',
+          'Кент',
+          'Клоун Красти',
+          'Ленни Леонард',
+          'Лионель Хатз',
+          'Лизякула',
+          'Мэгги',
+          'Мардж',
+          'Мартин Принц',
+          'Мэр Кимби',
+          'Милхаус ван Хутен',
+          'Мисс Хувер',
+          'Мо Сизлок',
+          'Нед Фландерс',
+          'Нельсон Мунтз',
+          'Отто Манн',
+          'Пэтти Бувер',
+          'Директор Скиннер',
+          'Фрик',
+          'Райнер Волкзамок',
+          'Ральф Виггум',
+          'Сельма Бувер',
+          'Сайдшоу Боб',
+          'Змея Птицаклетка',
+          'Трой Маккларен',
+          'Вейлон Смиттерс']
+
+model = ConvNN()
+model.load_state_dict(torch.load('model.pth'))
+model.eval()
+
+image = Image.open('./Data/kaggle_simpson_testset/kaggle_simpson_testset/comic_book_guy_1.jpg')
+plt.imshow(image)
+plt.show
+image = trans(image).unsqueeze(0).cuda()
+with torch.no_grad():
+  output = model(image)
+pred = (output.argmax(dim=1)).to('cpu').numpy()
+print(labels[pred[0]])
---
+++ Classification/train.py
@@ -0,0 +1,118 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+import shutil
+import os
+
+
+shutil.rmtree("testset")
+os.mkdir("testset")
+test_data_name = os.listdir("./Data/kaggle_simpson_testset/kaggle_simpson_testset")
+for i in range(len(test_data_name)):
+  name = test_data_name[i]
+  dst = "testset/"+ name[:name.rfind('_')]
+  path = "./Data/kaggle_simpson_testset/kaggle_simpson_testset/"+name
+
+  if not os.path.exists(dst):
+    os.mkdir(dst)
+  shutil.copyfile(path, dst+"/"+name)
+
+
+content = os.listdir('./Data/simpsons_dataset')
+content.sort()
+
+for i in content:
+    dst = "testset/"+i
+    src = "./Data/simpsons_dataset/"+i
+    file_name = os.listdir(src)[0]
+    src =src +  "/" + os.listdir(src)[0]
+
+    print(src)
+    if not os.path.exists(dst):
+      os.mkdir(dst)
+      dst = dst + "/" + file_name
+      shutil.copyfile(src,dst)
+
+class ConvNN(nn.Module):
+  def __init__(self):
+    super().__init__()
+    self.conv1 = nn.Conv2d(3, 32, kernel_size=(3,3))
+    self.conv2 = nn.Conv2d(32, 64, kernel_size=(3,3))
+    # self.f1 = nn.Flatten()
+    self.fc1 = nn.Linear(64*124*124, 128)
+    self.fc2 = nn.Linear(128, 42)
+
+  def forward(self, x):
+    x = F.relu(self.conv1(x))
+    x = F.relu(self.conv2(x))
+    # x = self.f1(x)
+    x = x.view(-1, 64*124*124)
+    x = F.relu(self.fc1(x))
+    x = self.fc2(x)
+    return x
+
+model = ConvNN().cuda()
+criterion = nn.CrossEntropyLoss().cuda()
+model
+
+data_path = "/Data/simpsons_dataset"
+trans = transforms.Compose([transforms.Resize((128, 128)), transforms.ToTensor()])
+dataset = datasets.ImageFolder(data_path, transform=trans)
+
+train_size = int(0.8 * len(dataset))
+val_size = len(dataset) - train_size
+train_dataset, val_dataset = torch.utils.data.random_split(dataset, [train_size, val_size])
+
+train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False)
+
+num_epochs = 30
+for epoch in range(num_epochs):
+  model.train()
+  optimizer = optim.SGD(model.parameters(), lr=0.001)#/(epoch+1)
+  running_loss = 0.0
+  TP = 0
+  TN = 0
+  FN = 0
+  FP = 0
+  for i, data in enumerate(train_loader,0):
+    inputs, labels = data
+    inputs=inputs.cuda()
+    labels=labels.cuda()
+    optimizer.zero_grad()
+    # print(i)
+    # print(labels)
+    outputs = model(inputs)
+    loss = criterion(outputs, labels)
+    loss.backward()
+    optimizer.step()
+    running_loss += loss.item()
+    real = sum(outputs.argmax(dim=1)==labels).to('cpu').item()
+    TP += real
+    TN += 40*32+real
+    FN += 32-real
+    FP += 32-real
+    model.eval()
+    val_loss = 0.0
+    correct = 0
+    total = 0
+  with torch.no_grad():
+    for i, data in enumerate(val_loader, 0):
+      inputs, labels = data
+      inputs = inputs.cuda()
+      labels = labels.cuda()
+      outputs = model(inputs)
+      loss = criterion(outputs, labels)
+      val_loss += loss.item()
+      _, predicted = torch.max(outputs,1)
+      total += labels.size(0)
+      correct += (predicted == labels).sum().item()
+  accuracy_per_epoch = ((TP+TN)/(TP+TN+FN+FP))
+  print(f'Epoch {epoch+1}/{num_epochs},Precision: {TP/(TP+FP)} , Train Loss: {running_loss/len(train_loader)},Train Accuracy: {accuracy_per_epoch}, Val Loss: {val_loss/len(val_loader)}, Val Accuracy: {100*correct/total}%')
+
+torch.save(model.state_dict(), 'model.pth')
+print("Модель сохранена в файл model.pth")
